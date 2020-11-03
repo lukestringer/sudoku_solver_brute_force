7c155e346e5649d7994ea5963c95d31e507413d8
lukestringer <luke.stringer@uqconnect.edu.au>
2020-11-03 23:47:40 +1000

    sudoku solver but I'm just getting started

---
+++ c.py
@@ -0,0 +1,20 @@
+
+
+'''
+111 111 111
+111 111 111
+111 111 111
+
+111 111 111
+111 111 111
+111 111 111
+
+111 111 111
+111 111 111
+111 111 111
+
+81 cells, each associated with a row, col, segment (9+8+4 = 21 cells)
+
+if cell is empty:
+    if row AND col AND seg dont contain number
+'''
