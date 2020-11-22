cb2c76a2e80b755410a95db4676a996e1772eaf3
lukestringer <luke.stringer@uqconnect.edu.au>
2020-11-23 00:29:14 +1000

    final commit with working script.\ngot rid of working materials (PyCharm does make a bit of mess, huh)

--- sudoku/e.py
+++
@@ -1,9 +0,0 @@
-
-for i in range(0,9):
-    for j in range(0,9):
-        print(i,"|", j, " ", end="", sep="")
-        if (j+1) % 3 == 0:
-            print("  ", end="")
-    print()
-    if (i+1) % 3 == 0:
-        print()
